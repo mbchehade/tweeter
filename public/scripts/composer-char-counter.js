c4bfc25b52aed45cd91e9c37f9333f196bc5ec63
mbchehade <57118552+mbchehade@users.noreply.github.com>
2020-01-29 16:54:59 -0700

    textarea character counter function jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  const maxLength = 140;
+  const $form = $('form');
+  const $counter = $('.counter');
+  $form.find('textarea')
+  .on("keyup", function(){
+    const character = maxLength - $(this).val().length
+    if($(this).val().length > maxLength){
+      $counter.css('color', 'red')
+      // $counter.addClass('red-color')
+    } else {
+      $counter.css('color', '#545149')
+    }
+    $counter.text(character)
+  })
+});
+
+// function at(strSelector) {
+//   return document.querySelector(strSelector)
+// }
