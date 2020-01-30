2854622adb7c75fd6e22c270148997753a7fa461
moe <mbchehade@gmail.com>
2020-01-30 16:05:30 +0000

    made post ajax request and checked for errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,61 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = function (tweets) {
-  $('#tweets-container').empty()
-  // loops through tweets
-  for (let item of tweets) {
-    // calls createTweetElement for each tweet
-    let newTweet = createTweetElement(item)
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').prepend(newTweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  }
-}
-
-
-const escape = function (str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-const createTweetElement = function (tweetData) {
-
-  const oneDay = 24 * 60 * 60 * 1000;
-  const tweetDate = new Date(tweetData["created_at"]);
-  const secondDate = new Date();
-  const diffDays = Math.round(Math.abs((tweetDate - secondDate) / oneDay));
-
-  const tweet = `<article class="tweet">
-        <header>
-          <section class="flexbox">
-            <div class="container1">
-              <img  src=${tweetData['user']['avatars']}>
-              <span class="name">${escape(tweetData['user']['name'])}</span>
-            </div>
-            <div class="container2">
-              <div>${tweetData['user']['handle']}</div>
-            </div>
-          </section>
-          <span class="quote">${escape(tweetData['content']['text'])}</span>
-        </header>
-        <footer>
-            <span class="date">${diffDays} days ago</span>
-        </footer>
-      </article>`
-  return tweet;
-}
-
-const loadTweets = function () {
-  $.ajax({
-    method: 'GET',
-    url: '/tweets'
-  })
-    .then(function (data) {
-      renderTweets(data);
-    })
-}
-
 $(function(){
   loadTweets();
   $('form').on('submit', function(event){
@@ -66,9 +11,9 @@ $(function(){
    const serializedData = $(this).serialize()
   console.log(serializedData)
    if(serializedData === "text="){
-     alert("You Should Type Something")
+     slideDown("Field Cannot be Empty")
    }else if(serializedData.length > 145){
-      alert("You Exceeded 140 Character")
+      slideDown("Please Respect the Arbitrary Limit of 140 Chars")
    }else{
       $.ajax({
         method: 'POST',
@@ -77,9 +22,7 @@ $(function(){
       }).then(function (testing) {
         console.log("success")
       });
-    
   loadTweets();
-
    }
   })
 });
