c7adf6eddad840d39d618477df6ec501ba14cc78
moe <mbchehade@gmail.com>
2020-01-31 02:23:52 +0000

    linted the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(function(){
+$(function() {
   loadTweets();
-  $('form').on('submit', function(event){
-    event.preventDefault()
-   const serializedData = $(this).serialize()
-  console.log(serializedData)
-   if(serializedData === "text="){
-     $(".error").text("Field Cannot be Empty")
-   }else if(serializedData.length > 145){
-      $(".error").text("Please Respect the Arbitrary Limit of 140 Chars")
-   }else{
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    console.log(serializedData);
+    if (serializedData === "text=") {
+      $(".error").text("Field Cannot be Empty");
+    } else if (serializedData.length > 145) {
+      $(".error").text("Please Respect the Arbitrary Limit of 140 Chars");
+    } else {
       $.ajax({
         method: 'POST',
         url: '/tweets',
         data: serializedData
-      }).then(function (testing) {
-        console.log("success")
+      }).then(function(testing) {
+        console.log("success");
       });
-  loadTweets();
-   }
-  })
+      loadTweets();
+    }
+  });
 });
 
 
 
-//when we are wrting jquery functions we are giving it an extra one. 
+//when we are wrting jquery functions we are giving it an extra one.
 //
