7bd264c5607cb4315ba8c62f42dcad160c0a83f1
moe <mbchehade@gmail.com>
2020-01-31 02:25:36 +0000

    linted the code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
   const maxLength = 140;
   const $form = $('form');
   const $counter = $('.counter');
   $form.find('textarea')
-  .on("keyup", function(){
-    const character = maxLength - $(this).val().length
-    if($(this).val().length > maxLength){
-      $counter.css('color', '#FF0000')
+    .on("keyup", function() {
+      const character = maxLength - $(this).val().length;
+      if ($(this).val().length > maxLength) {
+        $counter.css('color', '#FF0000');
       // $counter.addClass('red-color')
-    } else {
-      $counter.css('color', '#545149')
-    }
-    $counter.text(character)
-  })
+      } else {
+        $counter.css('color', '#545149');
+      }
+      $counter.text(character);
+    });
 });
 
 // function at(strSelector) {
